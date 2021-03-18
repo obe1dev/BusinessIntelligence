1c50fe09a1dc61a10b91576fad30a9ac7a5ea274
Brock Oberhansley <obe1.dev@gmail.com>
2021-03-17 22:33:38 -0600

    sort revenue data to charted.

--- screens/BusinessDetail.js
+++ screens/BusinessDetail.js
@@ -1,8 +1,28 @@
-import * as React from 'react';
-import {View} from 'react-native';
+import React, {useLayoutEffect} from 'react';
+import {View, Text} from 'react-native';
+import moment from 'moment';
+import {get} from 'lodash';
 
-export default class BusinessDetail extends React.Component {
-  render() {
-    return <View />;
-  }
+export default function BusinessDetail({navigation, route}) {
+  const revenue = route.params?.revenue ?? null;
+  let revValue = [];
+  let revDate = [];
+  revenue.map((item) => {
+    if (get(item, 'date') && get(item, 'value')) {
+      const parDate = moment(item.date).format('MMM YY');
+      revValue.push(item.value);
+      revDate.push(parDate);
+    }
+  });
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: route.params?.name,
+    });
+  }, [navigation, route.params]);
+
+  return (
+    <View>
+      <Text>cool</Text>
+    </View>
+  );
 }
